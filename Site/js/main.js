df3befe7c305be40113826c9fbda40bb9b7511b7
zeemclean <zoeakmclean@gmail.com>
2014-08-07 20:40:39 +1200

    fairly responsive

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
 
-	$(window).on('resize', function() {
-		//set the variable of the window,
-		var self = $(this);
-		//get the width of the window as it resizes,
-		var winWidth = self.outerWidth();
-		//check if the window width is greater or equal to 768px,
-		if ( winWidth >= 768 ) {
-			//if it is, remove the style attribute from the nav.
-			$('#tab_nav').removeAttr('style');
-		}
-
-
+	$('.drink_tab').on('click', function(event) {
+		event.preventDefault();
+		$('#food').slideUp('slow');
+		$('#drink').slideDown('slow');
 	});
 });
